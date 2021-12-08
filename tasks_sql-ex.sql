cdc135bd0201d0584f2eb19755f14df05072e2a3
nadya.chervyakova <kraynova06@yandex.ru>
2021-12-08 16:38:13 +0300

    49

--- tasks_sql-ex.sql
+++ tasks_sql-ex.sql
@@ -466,3 +466,23 @@ WHERE result = 'sunk';
 class
 Bismarck
 Kongo
+----------------------------------------------------------------------
+Задание: 49 
+Найдите названия кораблей с орудиями калибра 16 дюймов (учесть корабли из таблицы Outcomes).
+
+SELECT name
+FROM Ships INNER JOIN Classes ON Classes.class = Ships.class 
+WHERE bore = 16
+UNION
+SELECT ship
+FROM Outcomes INNER JOIN Classes ON Classes.class = Outcomes.ship
+WHERE bore = 16;
+
+name
+Iowa
+Missouri
+New Jersey
+North Carolina
+South Dakota
+Washington
+Wisconsin
